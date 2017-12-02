0bb0c0d5264dec86334f6931ba3c7ed26d3ecc39
Bartosz Górka <bartosz.gorka@appunite.com>
2017-12-02 21:00:55 +0100

    Show image with convert BGR to RGB

--- main.py
+++ main.py
@@ -1,5 +1,24 @@
 import cv2
 import glob
+import os
+import numpy as np
+from matplotlib import pyplot as plt
+
+
+def show_image(image, gray = True):
+    b, g, r = cv2.split(image)
+    rgb_image = cv2.merge([r,g,b])
+
+    if gray == True:
+        plt.imshow(rgb_image, cmap = plt.cm.gray)
+    else:
+        plt.imshow(rgb_image)
+
+    plt.show()
+
+
+def detect_edges(image):
+    return image
 
 
 if __name__ == '__main__':
@@ -10,7 +29,12 @@ if __name__ == '__main__':
       os.makedirs(results_dir)
 
     # Find files to read
-    files_name_list = glob.glob("data/*")
+    files_name_list = glob.glob("data/picture_003*")
+    # files_name_list = glob.glob("data/*")
 
     # Read files
     image_list = list(map(cv2.imread, files_name_list))
+
+    # Iterate on images
+    for image in image_list:
+        show_image(image)
