ea2b0ba3ca40f1762bbc142df6b879503fe01888
Bartosz Górka <bartosz.gorka@appunite.com>
2017-12-03 17:29:55 +0100

    Update min limit of rectangle area

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ def find_rectangle(img):
             for cnt in contours:
                 cnt_len = cv2.arcLength(cnt, True)
                 cnt = cv2.approxPolyDP(cnt, 0.02*cnt_len, True)
-                if len(cnt) == 4 and cnt.all() != 0 and cv2.contourArea(cnt) > 30000 and cv2.isContourConvex(cnt):
+                if len(cnt) == 4 and cnt.all() != 0 and cv2.contourArea(cnt) > 110000 and cv2.isContourConvex(cnt):
                     cnt = cnt.reshape(-1, 2)
                     max_cos = np.max([angle_cos( cnt[i], cnt[(i+1) % 4], cnt[(i+2) % 4] ) for i in range(4)])
                     if max_cos < 0.05:
